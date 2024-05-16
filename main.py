98d8fc071431e7c9296a6a921f66d756c7dcd6e1
Lucar <novodvorskij01@mail.ru>
2024-05-16 14:46:38 +0300

    A program that parses a website based on cryptocurrency news and saves data to a database

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 import asyncio
 import sqlite3
+import random
 from aiogram import Bot, types
 
 API_TOKEN = '6920587354:AAEMcoGh9s0BVFUWQ6gLNpcN3aSTEwUnv68'
 CHANNEL_ID = -1001724660274
 bot = Bot(token=API_TOKEN)
 
+EMOJIS = ['💸', '💰', '👀', '🔥']  # Add more emojis as needed
 
-async def send_message_with_photo(channel_id: int, text: str, photo_link: str):
-    await bot.send_photo(channel_id, photo=photo_link, caption=text, parse_mode=types.ParseMode.HTML)
+async def random_emoji():
+    return random.choice(EMOJIS)
 
+async def send_news_message(channel_id: int, text: str, photo_link: str):
+    try:
+        await bot.send_photo(channel_id, photo=photo_link, caption=text, parse_mode=types.ParseMode.HTML)
+    except Exception as e:
+        print(f"Error sending message: {e}")
 
-async def check_for_new_news():
-    # Создаем подключение к базе данных
+async def monitor_news():
     conn = sqlite3.connect('news.db')
     c = conn.cursor()
 
     while True:
-        # Получаем все новости из базы данных, у которых значение published равно 'no'
-        c.execute("SELECT * FROM News WHERE published=?", ('0',))
-        new_news_list = c.fetchall()
+        try:
+            c.execute("SELECT * FROM News WHERE published=?", ('0',))
+            new_news_list = c.fetchall()
 
-        for new_news in new_news_list:
-            # Опубликовываем новость в канале
-            news_text = f"💸💰<b>{new_news[0]}.</b>💸💰\n\n{new_news[2]}"
-            await send_message_with_photo(CHANNEL_ID, news_text, new_news[3])  # Отправляем сообщение с фотографией
+            for new_news in new_news_list:
+                emoji = await random_emoji()
+                news_text = f"{emoji}<b>{new_news[0]}.</b>{emoji}\n\n{new_news[2]}"
+                await send_news_message(CHANNEL_ID, news_text, new_news[3])
 
-            # Обновляем статус новости в базе данных на "опубликовано в канале"
-            c.execute("UPDATE News SET published=? WHERE link=?", ('1', new_news[1]))
-            conn.commit()  # Фиксируем изменения в базе данных
+                c.execute("UPDATE News SET published=? WHERE link=?", ('1', new_news[1]))
+                conn.commit()
+        except sqlite3.Error as e:
+            print(f"Error accessing database: {e}")
+        except Exception as e:
+            print(f"Error monitoring news: {e}")
 
-        await asyncio.sleep(420)  # Ожидание одного часа перед следующей проверкой
+        await asyncio.sleep(420)  # Wait for 1 hour before checking again
 
 if __name__ == '__main__':
-    asyncio.run(check_for_new_news())
+    asyncio.run(monitor_news())
